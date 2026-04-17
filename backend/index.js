09bc28e067a1fa9f0168e9253cdc9d5809237273
KarlaGMR-2004 <guadalupemr.ti22@utsjr.edu.mx>
2026-04-17 16:05:34 -0600

    fix render port

--- backend/index.js
+++ backend/index.js
@@ -95,9 +95,11 @@ app.get("/test", (req, res) =>
 );
 
 // SERVIDOR
-app.listen(3000, () =>
-  console.log("Servidor corriendo en http://localhost:3000")
-);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
 
 app.use((err, req, res, next) => {
   console.error("ERROR GLOBAL:", err);
